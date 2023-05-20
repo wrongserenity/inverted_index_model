b081090ae9c7abef3f4bba941a2357fdb7f01b6e
rewqqx <rewqqx@gmail.com>
2023-05-20 14:16:09 +0300

    Update elias_decoding.py

--- elias_decoding.py
+++ elias_decoding.py
@@ -23,27 +23,47 @@ def elias_gamma_decoding(x):
     return int(n)
 
 
-def elias_delta_decoding(x):
-    x = list(x)
-    L = 0
-    while True:
-        if not x[L] == '0':
-            break
-        L = L + 1
-
-    # Reading L more bits and dropping ALL
-    x = x[2 * L + 1:]
-
-    # Prepending with 1 in MSB
-    x.reverse()
-    x.insert(0, '1')
-    n = 0
-
-    # Converting binary to integer
+def decode(x):
+    num = 0
     for i in range(len(x)):
-        if x[i] == '1':
-            n = n + math.pow(2, i)
-    return int(n)
+        num += (int(x[len(x) - 1 - i]) * (math.pow(2, i)))
+    return num
+
+
+def elias_delta_decoding(x):
+    if x == '1':
+        return 1
+    else:
+        x = list(x)
+        t = 0
+        v = []
+        b = 0
+        w = []
+        c = 0
+        for i in x:
+            if b != 1:
+                if i == '0':
+                    t += 1
+                else:
+                    v.append(i)
+                    b = 1
+            elif c != 1:
+                if t == 0:
+                    c = 1
+                    w.append('1')
+                    w.append(i)
+                else:
+                    v.append(i)
+                    t -= 1
+            else:
+                num = decode(v)
+                if num == 0:
+                    break
+                else:
+                    w.append(i)
+                    num -= 1
+        ans = decode(w)
+    return int(ans)
 
 
 def elias_decoding(x, encoding):
